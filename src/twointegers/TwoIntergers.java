6b06541253efd9deddd3a7cb3f217d8bc6bf7c13
bob <bob@APCSA>
2017-09-15 14:51:28 -0700

    started it, started variables one and two and made them local

---
+++ src/twointegers/TwoIntergers.java
@@ -0,0 +1,34 @@
+package twointegers;thi
+
+public class TwoIntergers 
+{
+	private int one;
+	private int two;
+	public twoIntergers()
+	{
+		this.one = 0;
+		this.two = 0;
+	}
+	public twoIntergers(int one, int two)
+	{
+		this.one = one;
+		this.two = two;
+	}
+	public int getOne()
+	{
+		return one;
+	}
+	public int getTwo()
+	{
+		return two;
+	}
+	public void setOne(int one)
+	{
+		this.one = one;
+	}
+	public void setTwo(int two)
+	{
+		this.two = two;
+	}
+	public void 
+}
